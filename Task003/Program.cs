a884f3b5e5b80c3872682a8bde7cb25305d6e47a
Leonid <leeonidix@gmail.com>
2022-07-19 11:54:49 +0500

    Task 003 Done

---
+++ Task003/Program.cs
@@ -0,0 +1,20 @@
+﻿void Cubing(double number)
+{
+    double res = 1;
+    double count = 0;
+    while (count < number)
+    {
+        res = Math.Pow(res, 3);
+        count++;
+        Console.Write(res + " ");
+        res = count;
+        res++;
+    }
+
+}
+
+Console.Write("Enter number: ");
+
+double num = double.Parse(Console.ReadLine());
+Console.Write(num + " -> ");
+Cubing(num);
