c570a167900e537e1fc4aa7c921642c7a8e6a462
Leonid <leeonidix@gmail.com>
2022-07-19 10:23:12 +0500

    Task 001 Done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool NumCompar(int a) //Разбивает число на составные и проверяет является ли число палиндромом.
+{
+    int[] array = new int[4];
+    array[0] = a / 10000;
+    array[1] = a / 1000 % 10;
+    array[2] = a % 100 / 10;
+    array[3] = a % 10;
+    if (array[0] == array[3] & array[1] == array[2]) return true;
+    else return false;
+}
+
+Console.Write("Enter a five-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (NumCompar(number))
+{
+    Console.WriteLine($"{number} -> Yes");
+}
+else
+{
+    Console.WriteLine($"{number} -> No");
+}
