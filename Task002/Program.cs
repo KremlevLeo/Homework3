c0f8a1c0ea0dedfeefb70174a3bc8c6e4af3bd10
Leonid <leeonidix@gmail.com>
2022-07-19 11:11:16 +0500

    Task 002 Done

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter point A coordinates");
+Console.Write("X = ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Y = ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Z = ");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter point B coordinates");
+Console.Write("X = ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Y = ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Z = ");
+double z2 = double.Parse(Console.ReadLine());
+
+double res = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+
+Console.WriteLine($"A({x1},{y1},{z1}); B({x2},{y2},{z2}) -> {res}");
